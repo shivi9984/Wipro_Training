02f7d10ac3560f48147161a02f12d080fe4f3b7a
shivi9984 <shivamshivhare666@gmail.com>
2025-08-12 16:22:06 +0530

    Day21 Activities Uploaded

---
+++ testDemo1/src/test/java/testDemo1/Demo1.java
@@ -0,0 +1,14 @@
+package testDemo1;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	
+	@Test
+	public void add() {
+		
+	}
+	public static void main(String[] args) {
+		System.out.println("Test NG");
+	}
+}
